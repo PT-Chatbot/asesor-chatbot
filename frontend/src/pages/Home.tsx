7372e216a3ca286a04682cb7b1ffb0ec4d0d5c8b
marcosd59 <damian.marcospool@gmail.com>
2024-06-23 13:36:23 -0500

    feat: Add Home page component

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,45 @@
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import TypingAnim from "../components/typer/TypingAnim";
+import Footer from "../components/footer/Footer";
+
+const Home = () => {
+  const theme = useTheme();
+  useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+          mt: 3,
+        }}
+      >
+        <Box>
+          <TypingAnim />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: { md: "row", xs: "column", sm: "column" },
+            gap: 5,
+            my: 10,
+          }}
+        >
+          <img
+            className="image-inverted rotate"
+            src="/images/openai.png"
+            alt="openai"
+            style={{ width: "200px", margin: "auto" }}
+          />
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default Home;
